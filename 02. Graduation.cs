985dc38c83f287a25dcbd688708c876a9e3e658d
bhaveto <ioan_123@abv.bg>
2018-11-03 21:48:43 +0200

    Add files via upload

---
+++ 02. Graduation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string name = Console.ReadLine();
+            int grade = 1;
+            double avgGrade = 0.00;
+
+            while(grade <= 12)
+            {
+                double evaluation = double.Parse(Console.ReadLine());
+                if(evaluation >= 4)
+                {
+                    grade++;
+                    avgGrade += evaluation;
+                }
+            }
+            Console.WriteLine($"{name} graduated. Average grade: {avgGrade/12:f2}");
+        }
+    }
+}
